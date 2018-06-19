9e126fc3b58fb0a67dc7618a0cc5626ccf91f6b0
Jibidus <jibidus@gmail.com>
2018-06-19 16:49:25 +0200

    Add Kotlin tests

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ With a simple test class (which contains 15 tests):
 
 - `Vintage` version takes between **5ms** and **10ms**
 - One `Jupiter` version takes **700ms** ⚠️, and others are between **18ms** and **28ms** 🐢
+- `Kotlin` versions with `Jupiter` are between **20ms** and **22ms** 🐢
 
 Produced with `JUnit 5.2.0` and `Mockito 2.19.0` on my laptop.
 
---
+++ src/test/java/com/sogilis/junit/mockito/JupiterKotlinTest1.kt
@@ -0,0 +1,122 @@
+package com.sogilis.junit.mockito
+
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+internal class JupiterKotlinTest1 {
+
+    @InjectMocks
+    private lateinit var testedClass: VintageTest1.TestedClass
+
+    @Mock
+    private lateinit  var dependency: VintageTest1.Dependency
+
+    class TestedClass(private val dependency: VintageTest1.Dependency) {
+
+        fun apply(input: String): String {
+            return dependency.apply(input)
+        }
+    }
+
+    class Dependency {
+        fun apply(input: String): String {
+            return input
+        }
+    }
+
+    @Test
+    fun test1() {
+        whenever(dependency.apply("input value 1")).thenReturn("output value 1")
+        assertEquals("output value 1", testedClass.apply("input value 1"))
+    }
+
+    @Test
+    fun test2() {
+        whenever(dependency.apply("input value 2")).thenReturn("output value 2")
+        assertEquals("output value 2", testedClass.apply("input value 2"))
+    }
+
+    @Test
+    fun test3() {
+        whenever(dependency.apply("input value 3")).thenReturn("output value 3")
+        assertEquals("output value 3", testedClass.apply("input value 3"))
+    }
+
+    @Test
+    fun test4() {
+        whenever(dependency.apply("input value 4")).thenReturn("output value 4")
+        assertEquals("output value 4", testedClass.apply("input value 4"))
+    }
+
+    @Test
+    fun test5() {
+        whenever(dependency.apply("input value 5")).thenReturn("output value 5")
+        assertEquals("output value 5", testedClass.apply("input value 5"))
+    }
+
+    @Test
+    fun test6() {
+        whenever(dependency.apply("input value 1")).thenReturn("output value 1")
+        assertEquals("output value 1", testedClass.apply("input value 1"))
+    }
+
+    @Test
+    fun test7() {
+        whenever(dependency.apply("input value 2")).thenReturn("output value 2")
+        assertEquals("output value 2", testedClass.apply("input value 2"))
+    }
+
+    @Test
+    fun test8() {
+        whenever(dependency.apply("input value 3")).thenReturn("output value 3")
+        assertEquals("output value 3", testedClass.apply("input value 3"))
+    }
+
+    @Test
+    fun test9() {
+        whenever(dependency.apply("input value 4")).thenReturn("output value 4")
+        assertEquals("output value 4", testedClass.apply("input value 4"))
+    }
+
+    @Test
+    fun test10() {
+        whenever(dependency.apply("input value 5")).thenReturn("output value 5")
+        assertEquals("output value 5", testedClass.apply("input value 5"))
+    }
+
+    @Test
+    fun test11() {
+        whenever(dependency.apply("input value 1")).thenReturn("output value 1")
+        assertEquals("output value 1", testedClass.apply("input value 1"))
+    }
+
+    @Test
+    fun test12() {
+        whenever(dependency.apply("input value 2")).thenReturn("output value 2")
+        assertEquals("output value 2", testedClass.apply("input value 2"))
+    }
+
+    @Test
+    fun test13() {
+        whenever(dependency.apply("input value 3")).thenReturn("output value 3")
+        assertEquals("output value 3", testedClass.apply("input value 3"))
+    }
+
+    @Test
+    fun test14() {
+        whenever(dependency.apply("input value 4")).thenReturn("output value 4")
+        assertEquals("output value 4", testedClass.apply("input value 4"))
+    }
+
+    @Test
+    fun test15() {
+        whenever(dependency.apply("input value 5")).thenReturn("output value 5")
+        assertEquals("output value 5", testedClass.apply("input value 5"))
+    }
+}
